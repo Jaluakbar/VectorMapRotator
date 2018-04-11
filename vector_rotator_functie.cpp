4ac01fb8a39eda2f63d1b779da52c894805be4cf
Jaluakbar <31691920+Jaluakbar@users.noreply.github.com>
2018-04-11 10:33:04 +0200

    Netjes Variable

--- vector_rotator_functie.cpp
+++ vector_rotator_functie.cpp
@@ -29,6 +29,8 @@ vector<vector<bool>> rotate_matrix(vector<vector<bool>> const & mtx, int degree)
 	vector<vector<int>> koordinaats = eentjes_vinder(mtx);
 	float koordinaat_x =0;
 	float koordinaat_y =0;
+	float oud_x = 0;
+	float oud_y = 0;
 	
 	for(unsigned int i =0;i<koordinaats.size();i++){ //nieuw vec leeg maken
 		int point_a = koordinaats[i][0];
@@ -37,11 +39,11 @@ vector<vector<bool>> rotate_matrix(vector<vector<bool>> const & mtx, int degree)
 	}
 									
 	for(unsigned int i=0;i<koordinaats.size();i++){ //nieuw koordinaten berekenen
-		koordinaats[i][0] = koordinaats[i][0] - vec_mid_breed;
-		koordinaats[i][1] = koordinaats[i][1] - vec_mid_lengte;
+		oud_x = koordinaats[i][0] - vec_mid_breed;
+		oud_y = koordinaats[i][1] - vec_mid_lengte;
 		
-		koordinaat_x = round((koordinaats[i][0] * cos((degree*M_PI)/180))+(koordinaats[i][1] * -sin((degree*M_PI)/180)));
-		koordinaat_y = round((koordinaats[i][0] * sin((degree*M_PI)/180)) + (koordinaats[i][1] * cos((degree*M_PI)/180)));
+		koordinaat_x = round((oud_x * cos((degree*M_PI)/180))+ (oud_y * -sin((degree*M_PI)/180)));
+		koordinaat_y = round((oud_x * sin((degree*M_PI)/180)) + (oud_y * cos((degree*M_PI)/180)));
 		
 		koordinaat_x +=vec_mid_breed;
 		koordinaat_y +=vec_mid_lengte;
@@ -53,16 +55,21 @@ vector<vector<bool>> rotate_matrix(vector<vector<bool>> const & mtx, int degree)
 	
 }
 
-
-/*int main(){
+/*
+int main(){
 	printf("hello world\n");
-	vector<vector<bool>> een = { {0,0,1,0,0},
-								{0,0,1,0,0},
-								{0,0,1,0,0},
-								{0,0,1,0,0},
-								{0,0,1,0,0}};
+	vector<vector<bool>> een = { {0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0},
+								{0,0,1,0,0,0,0,0,0,0}};
 	
-	vector<vector<bool>> twee = rotate_matrix(een,30);
+	vector<vector<bool>> twee = rotate_matrix(een,45);
 	
 	vector<vector<int>> locatie = eentjes_vinder(een);
 	
