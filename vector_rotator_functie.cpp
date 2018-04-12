685ce7a2b08e19d1113672c17a74edef82fdce6e
Jaluakbar <31691920+Jaluakbar@users.noreply.github.com>
2018-04-12 11:36:49 +0200

    Define MPI OUT!

--- vector_rotator_functie.cpp
+++ vector_rotator_functie.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <math.h>
 #include "vector_rotator_functie.hpp"
-#define M_PI 3.14159265358979323846264338327950288
 using namespace std;
 
 vector<vector<int>> eentjes_vinder(vector<vector<bool>> const & vec){
