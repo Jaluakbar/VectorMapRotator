421d8d0689e23375902fd3db4cd185f3f6fe2b76
Jaluakbar <31691920+Jaluakbar@users.noreply.github.com>
2018-04-12 11:26:50 +0200

    HPP

---
+++ vector_rotator_functie.hpp
@@ -0,0 +1,11 @@
+#ifndef VectorRotate_HPP
+#define VectorRotate_HPP
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+vector<vector<bool>> rotate_matrix(vector<vector<bool>> const &, int);
+
+#endif
